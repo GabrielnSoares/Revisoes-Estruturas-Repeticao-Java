100d852dbc89ee08520b6ac74bf55eb8f578dcad
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-01-15 22:19:52 -0300

    exercicio DoWhile melhorias

--- src/br/gabnsoares/dev/aula21/exercicios/DoWhile.java
+++ src/br/gabnsoares/dev/aula21/exercicios/DoWhile.java
@@ -6,27 +6,39 @@ public class DoWhile {
 
     public static void main(String[] args) {
 
+        System.out.println("## JIRA CLI ##");
         Scanner sc  = new Scanner(System.in);
-
         String resposta;
 
         do {
             System.out.println("""
-                    ## JIRA CLI ##
                     1 - CRIAR TAREFA
                     2 - CRIAR PROJETO
                     3 - SAIR
                     DIGITE UMA OPÇÃO:\s""");
             resposta = sc.nextLine();
 
-            if (resposta.equalsIgnoreCase("1")) {
-                System.out.println("Digite o nome da tarefa: ");
-                String registro = sc.nextLine();
-                System.out.println(registro + " criada com sucesso!");
-            } else if (resposta.equalsIgnoreCase("2")) {
-                System.out.println("Digite o nome do projeto: ");
-                String registro = sc.nextLine();
-                System.out.println(registro + " criado com sucesso!");
+            switch (resposta) {
+                case "1":
+                    // cria tarefa
+                    System.out.println("Digite o nome da tarefa: ");
+                    String nomeTarefa = sc.nextLine();
+                    System.out.println("Tarefa [" + nomeTarefa + "] criada com sucesso!");
+                    break;
+                case "2":
+                    // cria projeto
+                    System.out.println("Digite o nome do projeto: ");
+                    String nomeProjeto = sc.nextLine();
+                    System.out.println("Projeto [" + nomeProjeto + "] criada com sucesso!");
+                    break;
+                case "3":
+                    // encerrar sistema
+                    System.out.println("Finalizando...");
+                    break;
+                default:
+                    // valor não mapeado
+                    System.out.println("Opção inválida!");
+                    break;
             }
 
         } while (!resposta.equalsIgnoreCase("3"));
